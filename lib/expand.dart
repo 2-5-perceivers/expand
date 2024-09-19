50c935daa9f79a8d5c6c45dc88a33c9bdfae8062
Rares Vanca <raresvanca@gmail.com>
2024-09-19 15:29:40 +0300

    Add library documentation

--- lib/expand.dart
+++ lib/expand.dart
@@ -1,3 +1,7 @@
+/// **expand** is a Flutter package designed to help creating sleek, fast and
+/// simple expandable widgets, using minimum code.
+library expand;
+
 export 'src/controller.dart';
 export 'src/expandable_card.dart';
 export 'src/expandable_tile.dart';
